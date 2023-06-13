6f6a87d3313fe387e9d575d1e0b0282aff78852f
Flavio Roman Goncalez <romanflavio2@gmail.com>
2023-06-13 11:17:30 -0400

    Create a footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <main>
-        <Card />
-      </main>
-      <footer className="attribution max-w-[500px] m-auto text-[1rem] text-White absolute left-0 right-0 bottom-0 mb-2">
-        Challenge by
-        <a
-          className="text-Cyan"
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by{" "}
-        <a className="text-Cyan ml-[2px]" href="#">
-          Flavio Roman Goncalez
-        </a>
-        .
-      </footer>
+      <Card />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="attribution max-w-[500px] m-auto text-[0.7rem] text-center text-White absolute left-0 right-0 bottom-0 mb-2">
+      Challenge by
+      <a
+        className="text-Cyan"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a className="text-Cyan ml-[2px]" href="#">
+        Flavio Roman Goncalez
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
